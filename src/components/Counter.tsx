a21cfb8c2a54450baa8bc6096697335d28cad594
David Woodward1 <david.i.woodward@disneystreaming.com>
2026-08-23 17:06:02 -0400

    Add tailwindcss and layout

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 export function Counter({ initial }: { initial: number }) {
   const [n, setN] = useState(initial);
   return (
-    <button type="button" onClick={() => setN((n) => n + 1)}>
+    <button
+      type="button"
+      onClick={() => setN((n) => n + 1)}
+      className="bg-blue-100 p-8"
+    >
       {n}
     </button>
   );
